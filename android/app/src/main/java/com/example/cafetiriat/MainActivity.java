59682f92ac804e4d58f5511393e8f50d5b01d1bf
Mahmoud-Nagy <mahmoud.naelhady@gmail.com>
2021-11-09 14:07:49 +0200

    initial commit

---
+++ android/app/src/main/java/com/example/cafetiriat/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.cafetiriat;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Cafeteria-T',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(),
+      );
+  }
+}
