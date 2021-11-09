226451e4624fa9ceb917bc85e8ead172e47270ba
Mahmoud-Nagy <mahmoud.naelhady@gmail.com>
2021-11-09 15:10:31 +0200

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:bloc/bloc.dart';
+import 'package:cafeteriat/shared/bloc-observer.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Bloc.observer = MyBlocObserver();
+
   runApp(const MyApp());
 }
 
